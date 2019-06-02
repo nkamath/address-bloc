c43c9892b8f7e112cc941b7de1700937e110cd33
Nritya Kamath <nritya.k@gmail.com>
2019-06-02 16:44:56 -0700

    checkpoint 2 complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -57,6 +57,10 @@ module.exports = class MenuController {
 
    }
 
+   getContactCount(){
+    return this.contacts.length;
+   }
+
    exit(){
      console.log("Thanks for using AddressBloc!");
      process.exit();
